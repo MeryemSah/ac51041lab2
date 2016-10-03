f2b474389c4224c97cf60eb40aeab6f83731cb7f
Mery <Mery@IBM121-PC01Y604>
2016-10-03 20:04:58 +0100

    d3 correct again

--- src/main/webapp/Scripts/d3script.js
+++ src/main/webapp/Scripts/d3script.js
@@ -1,5 +1,5 @@
 function d3draw() {
-    dataset=[2,5,8];    
+    var dataset=[2,5,8];    
     
     d3.select("body").selectAll("h1").data(dataset).enter().append("h1").text("hello it's Fahd!");
 }
