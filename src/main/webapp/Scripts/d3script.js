884ca752809f585be7c88597a5185f307d1475df
Mery <Mery@IBM121-PC01Y604>
2016-10-03 21:18:36 +0100

    circles

--- src/main/webapp/Scripts/d3script.js
+++ src/main/webapp/Scripts/d3script.js
@@ -11,7 +11,7 @@ window.onload=d3draw;
 
 function drawCircle()
 {
-    var data=[[100,20],[45,55],[225,355]];
+    var data=[[10,20,46],[45,55,20],[25,35,15]];
     
     var h = 500;
     var w = 600;
@@ -20,7 +20,7 @@ function drawCircle()
     
     var circles = canvas.selectAll("circle").data(data).enter().append("circle");
     
-    circles.attr("cx",function (d){return d[0];}).attr("cy",function(d){return d[1];}).attr("r",6);
+    circles.attr("cx",function (d){return d[0];}).attr("cy",function(d){return d[1];}).attr("r", function(d){return d[2];});
 }
 window.onload=drawCircle;
 
