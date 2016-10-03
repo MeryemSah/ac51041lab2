cde948903ea2ee8b62e9ccf9d9e95bcd9ac9cc24
Mery <Mery@IBM121-PC01Y604>
2016-10-03 20:14:34 +0100

    anonymous func

--- src/main/webapp/Scripts/d3script.js
+++ src/main/webapp/Scripts/d3script.js
@@ -1,6 +1,8 @@
 function d3draw() {
-    var dataset=[2,5,8];    
+    var dataset=[2,5,8, 16, 20, 71, 29];    
     
-    d3.select("body").selectAll("h1").data(dataset).enter().append("h1").text("hello it's Fahd!");
+    d3.select("body").selectAll("h1").data(dataset).enter().append("h1").text(function (d){
+        return "This is number " + d;
+    });
 }
 window.onload=d3draw;
